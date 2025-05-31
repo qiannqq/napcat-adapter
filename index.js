02b4bef5ba57e36f8a8f2f3bcbe5c31f9abf3597
千奈千祁 <2632139786@qq.com>
2025-05-31 15:20:50 +0800

    feat: 自动更新缺失的配置文件项

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import ncadapter from "./adapter/index.js";
-import { cfg } from './lib/index.js'
 import { nccommon } from "./lib/index.js";
+
+let { cfg } = await import('./lib/index.js')
 
 let version = nccommon.getNaVersion()
 logger.mark(`===============`)
@@ -8,8 +9,8 @@ logger.mark(`${logger.blue('[NapCat-Adapter]')} version ${version}`)
 logger.mark(`${logger.blue(`[NapCat-Adapter]`)} repository https://github.com/qiannqq/napcat-adapter`)
 logger.mark(`===============`)
 
+/** 创建nc实例并初始化 */
 const nc = new ncadapter(cfg());
-
 nc.init()
 
 let ncu = await import('./other/u.js')
